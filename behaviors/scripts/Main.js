3d85b08ffc80dd5069fe8070cff16cac1c726b2e
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-03-29 22:45:15 +0800

    more things aaaaaaaa

--- behaviors/scripts/Main.js
+++ behaviors/scripts/Main.js
@@ -1,1 +1,6 @@
 import "scripts/DirewolfSniff.js";
+import { world } from "mojang-minecraft";
+
+world.events.tick.subscribe((ev) => {
+    world.getDimension('overworld').runCommand(`event entity @a rift:remove_warning`);
+});
