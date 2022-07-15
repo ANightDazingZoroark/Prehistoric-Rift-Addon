aeb95293755b3ec780cfd9de03d6987e171fb8f8
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-07-15 12:27:29 +0800

    more eating from inventory stuff

--- behaviors/scripts/AbilityManagement.js
+++ behaviors/scripts/AbilityManagement.js
@@ -3,18 +3,16 @@ import { EntityQueryOptions, world } from "mojang-minecraft"
 const filters = new EntityQueryOptions()
 filters.type = 'rift:saurophaganax'
 
-let lightBlastNotify = true
-
 world.events.tick.subscribe((ev) => {
     let entities = Array.from(world.getDimension('overworld').getEntities(filters))
     for (let i = 0; i < entities.length; i++) {
         let score = world.scoreboard.getObjective('saurophLightBlst').getScore(entities[i].scoreboard)
-        if (score >= 10 && lightBlastNotify == true) {
+        if (score >= 10 && entities[i].hasTag('canNotify')) {
             entities[i].runCommand(`tellraw @p {"rawtext":[{"text":"Light Blast Available!"}]}`)
-            lightBlastNotify = false
+            entities[i].runCommand(`event entity @s rift:cannot_notify`)
         }
         if (score < 10) {
-            lightBlastNotify = true
+            entities[i].runCommand(`event entity @s rift:can_notify`)
         }
     }
 })
--- behaviors/scripts/EatFromInventory.js
+++ behaviors/scripts/EatFromInventory.js
@@ -1,5 +1,8 @@
 import { EntityQueryOptions, world } from "mojang-minecraft"
 
+let filter = new EntityQueryOptions();
+filter.excludeTags = ['eatFromInventoryCooldown']
+
 const validMobs = [
     'rift:tyrannosaurus',
     'rift:stegosaurus',
@@ -14,28 +17,69 @@ const validMobs = [
     'rift:baryonyx'
 ]
 
-const filters = new EntityQueryOptions()
-
 world.events.tick.subscribe((ev) => {
-    let entities = Array.from(world.getDimension('overworld').getEntities(filters))
+    let entities = Array.from(world.getDimension('overworld').getEntities(filter))
     for (let i = 0; i < entities.length; i++) {
-        if (entities[i].getComponent('health').current < entities[i].getComponent('health').value && validMobs.includes(entities[i].id) ){ 
-            for (let j = entities[i].getComponent('inventory').inventorySize - 1; j >= 0; j--) {
-                try {
-                    for (let k = 0; k < Object.keys(entities[i].getComponent('healable').items).length; k++) {
-                        if (entities[i].getComponent('inventory').container.getItem(j).id == entities[i].getComponent('healable').items[k].item && entities[i].getComponent('inventory').container.getItem(j).amount > 1) {
-                            entities[i].getComponent('health').setCurrent(entities[i].getComponent('health').current + entities[i].getComponent('healable').items[k].healAmount)
-                            entities[i].runCommand(`replaceitem entity @s slot.inventory `+j+` `+entities[i].getComponent('healable').items[k].item+` `+(entities[i].getComponent('inventory').container.getItem(j).amount-1).toString()+` 0`)
+        try {
+            if (entities[i].getComponent('health').current < entities[i].getComponent('health').value && validMobs.includes(entities[i].id) ){ 
+                for (let j = entities[i].getComponent('inventory').inventorySize - 1; j >= 0; j--) {
+                    try {
+                        if (entities[i].getComponent('healable').items.map(x => x.item).includes(entities[i].getComponent('inventory').container.getItem(j).id)) {
+                            try {
+                                for (let k = 0; k < Object.keys(entities[i].getComponent('healable').items).length; k++) {
+                                    if (entities[i].getComponent('inventory').container.getItem(j).id == entities[i].getComponent('healable').items[k].item && entities[i].getComponent('inventory').container.getItem(j).amount > 1) {
+                                        entities[i].getComponent('health').setCurrent(entities[i].getComponent('health').current + entities[i].getComponent('healable').items[k].healAmount)
+                                        entities[i].runCommand(`replaceitem entity @s slot.inventory `+j+` `+entities[i].getComponent('healable').items[k].item+` `+(entities[i].getComponent('inventory').container.getItem(j).amount-1).toString()+` 0`)
+                                    }
+                                    else if (entities[i].getComponent('inventory').container.getItem(j).id == entities[i].getComponent('healable').items[k].item && entities[i].getComponent('inventory').container.getItem(j).amount == 1) {
+                                        entities[i].getComponent('health').setCurrent(entities[i].getComponent('health').current + entities[i].getComponent('healable').items[k].healAmount)
+                                        entities[i].runCommand(`replaceitem entity @s slot.inventory `+j+` air 1 0`)
+                                    }
+                                    try {
+                                        switch (entities[i].getComponent('inventory').container.getItem(j).id) {
+                                            case "rift:cooked_dodo_meat":
+                                                entities[i].runCommand(`effect @s absorption 30 3`)
+                                                break
+                                            case "rift:cooked_hadrosaur_meat":
+                                                entities[i].runCommand(`effect @s resistance 60 3`)
+                                                break
+                                            case "rift:mixed_meat":
+                                                entities[i].runCommand(`effect @s absorption 120`)
+                                                entities[i].runCommand(`effect @s regeneration 5 1`)
+                                                break
+                                            case "minecraft:golden_apple":
+                                                entities[i].runCommand(`effect @s absorption 120`)
+                                                entities[i].runCommand(`effect @s regeneration 5 1`)
+                                                break
+                                            case "rift:enchanted_mixed_meat":
+                                                entities[i].runCommand(`effect @s absorption 120 3`)
+                                                entities[i].runCommand(`effect @s regeneration 30 1`)
+                                                entities[i].runCommand(`effect @s fire_resistance 300`)
+                                                entities[i].runCommand(`effect @s resistance 300`)
+                                                break
+                                            case "minecraft:enchanted_golden_apple":
+                                                entities[i].runCommand(`effect @s absorption 120 3`)
+                                                entities[i].runCommand(`effect @s regeneration 30 1`)
+                                                entities[i].runCommand(`effect @s fire_resistance 300`)
+                                                entities[i].runCommand(`effect @s resistance 300`)
+                                                break
+                                        }
+                                    }
+                                    catch (e) {}
+                                    entities[i].runCommand(`event entity @s rift:start_eat_from_inventory_cooldown`)
+                                }
+                                break
+                            }
+                            catch (e) {}
                         }
-                        else if (entities[i].getComponent('inventory').container.getItem(j).id == entities[i].getComponent('healable').items[k].item && entities[i].getComponent('inventory').container.getItem(j).amount == 1) {
-                            entities[i].getComponent('health').setCurrent(entities[i].getComponent('health').current + entities[i].getComponent('healable').items[k].healAmount)
-                            entities[i].runCommand(`replaceitem entity @s slot.inventory `+j+` air 1 0`)
+                        else {
+                            continue
                         }
                     }
-                    break
+                    catch (e) {}
                 }
-                catch (e) {}
             }
         }
+        catch (e) {}
     }
 })
