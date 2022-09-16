928a27e2140d6d1b1f3c7f2973943bfeb10da564
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-09-16 10:20:39 +0800

    parasaurolophus pyrophobia

--- behaviors/scripts/BlockDetectManagement.js
+++ behaviors/scripts/BlockDetectManagement.js
@@ -10,7 +10,7 @@ let fireBlocks = [
 ]
 
 world.events.tick.subscribe((ev) => {
-    if (ev.currentTick % 5 == 0) {
+    if (ev.currentTick % 20 == 0) {
         let entities = Array.from(world.getDimension('overworld').getEntities())
         mainloop: for (let e = 0; e < entities.length; e++) {
             if (entities[e].id == 'rift:tyrannosaurus' && !entities[e].hasTag('tamed') && !entities[e].hasTag('angeredByFire') && !entities[e].hasTag('sleeping') && !entities[e].hasTag('baby')) {
@@ -19,6 +19,32 @@ world.events.tick.subscribe((ev) => {
                         for (let z = -12; z < 13; z++) {
                             if (fireBlocks.includes(entities[e].dimension.getBlock(new BlockLocation(Math.trunc(entities[e].location.x+x), Math.trunc(entities[e].location.y+y), Math.trunc(entities[e].location.z+z))).id)) {
                                 entities[e].triggerEvent('rift:angered_by_fire')
+                                continue mainloop
+                            }
+                        }
+                    }
+                }
+            }
+            if (entities[e].id == 'rift:parasaurolophus' && !entities[e].hasTag('tamed') && !entities[e].hasTag('angeredByFire') && !entities[e].hasTag('baby') && !entities[e].target) {
+                for(let x = -12; x < 13; x++) {
+                    for (let y = -2; y < 4; y++){
+                        for (let z = -12; z < 13; z++) {
+                            if (fireBlocks.includes(entities[e].dimension.getBlock(new BlockLocation(Math.trunc(entities[e].location.x+x), Math.trunc(entities[e].location.y+y), Math.trunc(entities[e].location.z+z))).id)) {
+                                entities[e].addTag('angeredByFire')
+                                entities[e].triggerEvent('rift:adult_call_and_become_angry')
+                                continue mainloop
+                            }
+                        }
+                    }
+                }
+            }
+            if (entities[e].id == 'rift:parasaurolophus' && !entities[e].hasTag('tamed') && !entities[e].hasTag('angeredByFire') && entities[e].hasTag('baby') && !entities[e].target) {
+                for(let x = -12; x < 13; x++) {
+                    for (let y = -2; y < 4; y++){
+                        for (let z = -12; z < 13; z++) {
+                            if (fireBlocks.includes(entities[e].dimension.getBlock(new BlockLocation(Math.trunc(entities[e].location.x+x), Math.trunc(entities[e].location.y+y), Math.trunc(entities[e].location.z+z))).id)) {
+                                entities[e].addTag('angeredByFire')
+                                entities[e].triggerEvent('rift:baby_become_scared')
                                 continue mainloop
                             }
                         }
