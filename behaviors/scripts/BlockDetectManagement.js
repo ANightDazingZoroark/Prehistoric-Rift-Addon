fa5fd57a66131f7f4639349444489cb9ee53c137
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2022-09-18 19:16:38 +0800

    finished all fire related interactions yay

--- behaviors/scripts/BlockDetectManagement.js
+++ behaviors/scripts/BlockDetectManagement.js
@@ -51,6 +51,18 @@ world.events.tick.subscribe((ev) => {
                     }
                 }
             }
+            if (entities[e].id == 'rift:direwolf' && !entities[e].hasTag('tamed') && !entities[e].hasTag('angeredByFire') && entities[e].hasTag('ignited') && !entities[e].hasTag('baby')) {
+                for(let x = -12; x < 13; x++) {
+                    for (let y = -2; y < 4; y++){
+                        for (let z = -12; z < 13; z++) {
+                            if (fireBlocks.includes(entities[e].dimension.getBlock(new BlockLocation(Math.trunc(entities[e].location.x+x), Math.trunc(entities[e].location.y+y), Math.trunc(entities[e].location.z+z))).id)) {
+                                entities[e].triggerEvent('rift:angered_by_fire')
+                                continue mainloop
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 })
