f3a9a3e8f0e2c82f69b6fea781ef34b82b8e4839
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2023-05-17 16:22:49 +0800

    le 3rd miniboss

--- behaviors/scripts/SpawnManagement.js
+++ behaviors/scripts/SpawnManagement.js
@@ -3,13 +3,16 @@ import { world } from "@minecraft/server"
 world.events.entitySpawn.subscribe((ev) => {
     switch (ev.entity.typeId) {
         case 'rift:cavern_megapiranha':
-            ev.entity.nameTag = '§4Cavern Megapiranha'
+            ev.entity.nameTag = '§4Cavern Megapiranha§r'
             break
         case 'rift:cavern_utahraptor':
-            ev.entity.nameTag = '§4Cavern Utahraptor'
+            ev.entity.nameTag = '§4Cavern Utahraptor§r'
             break
         case 'rift:cavern_dilophosaurus':
-            ev.entity.nameTag = '§4Cavern Dilophosaurus'
+            ev.entity.nameTag = '§4Cavern Dilophosaurus§r'
+            break
+        case 'rift:cavern_boss':
+            ev.entity.nameTag = '§5§kaaaaaaaaaa§r'
             break
     }
 })
