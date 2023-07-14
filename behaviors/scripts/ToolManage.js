5e6fe2470b0f585a9b031f0681efa9bef16bcaaa
ANightDazingZoroark <56536620+ANightDazingZoroark@users.noreply.github.com>
2023-07-14 11:53:55 +0800

    two tools added

--- behaviors/scripts/ToolManage.js
+++ behaviors/scripts/ToolManage.js
@@ -1,4 +1,4 @@
-import { system, ItemStack, ItemTypes, world } from "@minecraft/server"
+import { system, EffectTypes, ItemStack, ItemTypes, world } from "@minecraft/server"
 
 let triceratopsSpearVul = [
     "rift:tyrannosaurus",
@@ -27,7 +27,6 @@ world.afterEvents.dataDrivenEntityTriggerEvent.subscribe((ev) => {
             durability.damage += 1
             if (durability.damage < durability.maxDurability) {
                 ev.entity.runCommandAsync('replaceitem entity @s[hasitem={item=rift:triceratops_shield, location=slot.weapon.mainhand}] slot.weapon.mainhand 0 rift:triceratops_shield 1 '+durability.damage)
-                // ev.entity.runCommandAsync('playsound item.shield.block @a[r=3]')
                 world.playSound('item.shield.block', ev.entity.location)
             }
             else {
@@ -52,11 +51,43 @@ world.afterEvents.dataDrivenEntityTriggerEvent.subscribe((ev) => {
         }
         catch (e) {}
     }
+
+    if (ev.id == 'rift:reduce_tenontosaurus_charmer_durability') {
+        try {
+            const mainHand = ev.entity.getComponent('equipment_inventory').getEquipment('mainhand')
+            const durability = mainHand.getComponent('durability')
+            durability.damage += 1
+            if (durability.damage < durability.maxDurability) {
+                ev.entity.runCommandAsync('replaceitem entity @s[hasitem={item=rift:tenontosaurus_charmer, location=slot.weapon.mainhand}] slot.weapon.mainhand 0 rift:tenontosaurus_charmer 1 '+durability.damage)
+            }
+            else {
+                ev.entity.getComponent('equipment_inventory').setEquipment('mainhand')
+                ev.entity.runCommandAsync('playsound random.break @a[r=3]')
+            }
+        }
+        catch (e) {}
+
+        try {
+            const offHand = ev.entity.getComponent('equipment_inventory').getEquipment('offHand')
+            const durability = offHand.getComponent('durability')
+            durability.damage += 1
+            if (durability.damage < durability.maxDurability) {
+                ev.entity.runCommandAsync('replaceitem entity @s[hasitem={item=rift:tenontosaurus_charmer, location=slot.weapon.offhand}] slot.weapon.offhand 0 rift:tenontosaurus_charmer 1 '+durability.damage)
+            }
+            else {
+                ev.entity.getComponent('equipment_inventory').setEquipment('offHand')
+                ev.entity.runCommandAsync('playsound random.break @a[r=3]')
+            }
+        }
+        catch (e) {}
+    }
 })
 
 world.afterEvents.entityHurt.subscribe((ev) => {
     const attacker = ev.damageSource.damagingEntity
     const attacked = ev.hurtEntity
+    const attackingProjectile = ev.damageSource.damagingProjectile
+
     if (attacker.getComponent('equipment_inventory').getEquipmentSlot('mainhand').typeId == 'rift:ankylosaurus_mace') {
         attacked.applyKnockback(attacker.getViewDirection().x, attacker.getViewDirection().z, 3, 0.25)
     }
@@ -65,6 +96,17 @@ world.afterEvents.entityHurt.subscribe((ev) => {
     }
     if (attacker.getComponent('equipment_inventory').getEquipmentSlot('mainhand').typeId == 'rift:triceratops_spear' && triceratopsSpearVul.includes(attacked.typeId)) {
         attacked.applyDamage(7)
+    }
+    if (attacker.getComponent('equipment_inventory').getEquipmentSlot('mainhand').typeId == 'rift:baryonyx_dagger') {
+        attacked.addEffect(EffectTypes.get("poison"), 200)
+    }
+    if (attackingProjectile.typeId == 'rift:thrown_triceratops_spear' && triceratopsSpearVul.includes(attacked.typeId)) {
+        attacked.applyDamage(7)
+        let remDurability = parseInt(ev.entity.getTags()[0].split('')[1])
+        if (remDurability >= 800) {
+            attackingProjectile.runCommandAsync('playsound random.break @a[r=3]')
+            attackingProjectile.triggerEvent('rift:commit_disappear')
+        }
     }
 })
 
